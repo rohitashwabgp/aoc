8b762ea8c2f6e17100b44aab4cc6e4886d4d05f9
rohitashwabgp <rohitashwabgp@gmail.com>
2023-12-09 12:24:32 +0530

    day 9

--- problem_9_2.js
+++ problem_9_2.js
@@ -1,14 +1,55 @@
 const fs = require('node:fs');
 
 
-function covert() {
+function convert() {
     let input = "";
     let input_cards = [];
-    input = fs.readFileSync('./input_7.txt', 'utf8');
+    input = fs.readFileSync('./input_9.txt', 'utf8');
     input = input.split("\n");
     input.forEach((data, index) => {
-        input_cards.push(data.trim().split(" "))
+        let temp = data.trim().split(" ")
+        temp = temp.map(data => +data)
+        input_cards.push(temp)
     })
 
-    return { input_cards };
+    return input_cards;
 }
+
+let input = convert();
+console.log(input);
+
+function next(check_list) {
+    let new_check_list = [];
+    for (let j = 0; j < check_list.length - 1; j++) {
+        new_check_list.push(check_list[j + 1] - check_list[j])
+    }
+    return new_check_list;
+}
+let next_item_list2 = []
+for (let item of input) {
+    let temp = item;
+    let next_item_list = [item];
+    while (true) {
+        if (temp.length > 0) {
+            temp = next(item);
+            item = temp
+            next_item_list.push(temp);
+            temp = temp.filter(it => it != 0);
+        } else {
+            break;
+        }
+    }
+    next_item_list2.push(next_item_list)
+}
+
+console.log(next_item_list2)
+
+let sum = 0;
+for (let item of next_item_list2) {
+    let last = 0;
+    for (let i = item.length - 1; i >= 0; i--) {
+        last = item[i][0] - last;
+    }
+    sum = sum + last;
+}
+console.log(sum)
